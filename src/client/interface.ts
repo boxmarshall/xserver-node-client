ecdcbd82845b4920bb4441d5d36ee681ea2164b5
David Ibia <david.ibia@boxmarshall.com>
2022-05-31 09:19:59 +0100

    fix: throw handle error

--- src/client/interface.ts
+++ src/client/interface.ts
@@ -1,64 +1,73 @@
 export interface XServerClientOptions {
-    apiKey: string;
-    baseUrl?: string;
+  apiKey: string
+  baseUrl?: string
 }
 
-
 export interface BaseLicenseOptions {
-    serialkey: string;
-    email: string;
+  serialkey: string
+  email: string
 }
 
 export interface ExtendLicenseKeyOptions {
-    serialkey: string;
-    days: number;
+  serialkey: string
+  days: number
 }
 
 export interface MetaLicenseKeyOptions {
-    serialkey: string;
-    meta: {
-        [key: string]: string;
-    }
+  serialkey: string
+  meta: {
+    [key: string]: string
+  }
 }
 
 export interface BanLicenseKeyOptions {
-    serialkey: string;
-    state: boolean;
+  serialkey: string
+  state: boolean
 }
 
 export interface BindDiscordUserOptions {
-    serialkey: string;
-    email: string;
-    discord: {
-        id: string;
-        username: string;
-        discriminator: string;
-        avatar: string;
-        isBot: boolean;
-        nitro: boolean;
-        email: string;
-        emailVerified: boolean;
-    }
+  serialkey: string
+  email: string
+  discord: {
+    id: string
+    username: string
+    discriminator: string
+    avatar: string
+    isBot: boolean
+    nitro: boolean
+    email: string
+    emailVerified: boolean
+  }
 }
 
 export interface LicenseOptions {
-    email: string;
-    givenName: string;
-    familyName: string;
-    tierHash?: string;
-    days: number;
-    meta?: MetaLicenseKeyOptions['meta'];
+  email: string
+  givenName: string
+  familyName: string
+  tierHash?: string
+  days: number
+  meta?: MetaLicenseKeyOptions['meta']
 }
 
-export interface EternalLicenseOptions extends LicenseOptions {
-
-}
-
+export interface EternalLicenseOptions extends LicenseOptions {}
 
 export interface ConfirmTransferOptions {
-    serialkey: string;
-    email: string;
-    familyName: string;
-    givenName: string;
-    transferCode: string;
+  serialkey: string
+  email: string
+  familyName: string
+  givenName: string
+  transferCode: string
+}
+
+export interface APIResponse {
+  status: string
+}
+
+export interface LicenseObject {
+  key: string
+  expiryDate: Date | string
+}
+
+export interface FetchLicenseResponse extends APIResponse {
+  keys: LicenseObject[]
 }
